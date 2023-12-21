ced406cd666ed0e13f9f00da2f68f9d71865b7d4
nainoktv <nainoaktvillegas@gmail.com>
2023-12-21 10:51:54 -0800

    while loop to select number of players

--- index.py
+++ index.py
@@ -7,5 +7,16 @@ def roll():
 
   return roll
 
-value = roll()
-print(value)
+
+while True:
+  players = input("Enter the number of players (2 - 4): ")
+  if players.isdigit():
+    players = int(players)
+    if 2 <= players <= 4:
+      break
+    else:
+      print("Must be between 2 to 4 players.")
+  else:
+    print("Invalid, try again.")
+
+print(players)
