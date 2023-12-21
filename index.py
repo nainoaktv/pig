ac34132543f3dc80b3f0d3b56b33660d23315aa1
nainoktv <nainoaktvillegas@gmail.com>
2023-12-21 11:31:38 -0800

    feat: increment score after every roll

--- index.py
+++ index.py
@@ -19,4 +19,28 @@ while True:
   else:
     print("Invalid, try again.")
 
-print(players)
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+  for player_idx in range(players): 
+    print("\nPlayer number", player_idx + 1, "turn has just started!\n")
+    current_score = 0
+
+    while True:
+      should_roll = input("Would you like to roll (y)? ")
+      if should_roll.lower() != "y":
+        break
+
+      value = roll()
+      if value == 1:
+        print("You rolled a 1! Turn Done.")
+        current_score = 0
+      else:
+        current_score += value
+        print("You rolled a:", value)
+
+      print("Your score is:", current_score)
+    
+    player_scores[player_idx] += current_score
+    print("Your total score is:", player_scores[player_idx])
